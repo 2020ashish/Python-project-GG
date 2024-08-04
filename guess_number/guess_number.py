bed78eb5eda714487b49bb1f794a8e009da17b35
2020ashish <arajanand29@gmail.com>
2024-08-04 11:37:52 +0530

    Guess the number

--- guess_number/guess_number.py
+++ guess_number/guess_number.py
@@ -1,32 +1,33 @@
-import random
 import math
+import random
+
 print("Enter a range of value")
-n1 =  int(input("Enter first number : "))
-n2 =  int(input("Enter Second number : "))
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter Second number : "))
 
 if n1 < n2:
-   n1 = n1 + n2 
-   n2 = n1 - n2
-   n1 = n1 - n2
-x =  random.randint(n2,n1)
-chances = math.ceil(math.log(n1-n2+1,2))
+    n1 = n1 + n2
+    n2 = n1 - n2
+    n1 = n1 - n2
+x = random.randint(n2, n1)
+chances = math.ceil(math.log(n1 - n2 + 1, 2))
 
-flag =  False
+flag = False
 count = 0
 print("\nYou've only ", chances, " chances to guess the integer!\n")
-while( count < chances):
-    count+=1
-    guess =  int(input("Enter guess number : "))
-    
-    if  guess == x:
+while count < chances:
+    count += 1
+    guess = int(input("Enter guess number : "))
+
+    if guess == x:
         print("Wow ! You guessed the correct number")
         flag = True
         break
-    elif (guess > x):
+    elif guess > x:
         print("Your guessed too high!")
     else:
         print("Your guessed too less!")
-        
+
 if not flag:
     print("\nThe number is %d" % x)
     print("\tBetter Luck Next time!")
